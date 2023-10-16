541085ea50355ce710d97d48a689a68b33ee9b5c
Agezadebelu <lateradeb2019@gmail.com>
2023-10-16 05:37:59 -0700

    Simple Shell

--- main.c
+++ main.c
@@ -7,39 +7,39 @@
 
 int main(void)
 {
-	int bytes_read = 0, loop = 0, exstat = 0;
+	int read = 0, loop = 0, exitstat = 0;
 	size_t size = 0;
 	char *str_line = NULL;
 	char **args = NULL;
 
 	while (loop == 0)
 	{
-		signal(SIGINT, ctrlC);
+		signal(SIGINT, hsh_ctrlC);
 		if (isatty(STDIN_FILENO) == 1)
 			write(STDOUT_FILENO, "$ ", 2);
-		bytes_read = getline(&str_line, &size, stdin);
-		if (bytes_read == -1)
+		read = getline(&str_line, &size, stdin);
+		if (read == -1)
 		{
 			if (isatty(STDIN_FILENO) == 1)
 				write(STDOUT_FILENO, "\n", 1);
 			break;
 		}
-		if (breakline(str_line) == 0)
+		if (hsh_breakline(str_line) == 0)
 			continue;
-		args = stringtok(str_line, " \t\n");
+		args = hsh_stringtok(str_line, " \t\n");
 		if (args[0] != NULL)
 		{
 			if (_strcmp(args[0], "exit") == 0)
 			{
-				exstat = exit_status(args);
-				if (exstat == 2)
+				exitstat = exit_status(args);
+				if (exitstat == 2)
 					continue;
 				free(str_line);
-				exit(exstat);
+				exit(exitstat);
 			}
-			if (builtin(args) == 1)
+			if (hsh_builtin(args) == 1)
 				continue;
-			execarg(args);
+			hsh_execarg(args);
 			free(args);
 		}
 		else
